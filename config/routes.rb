589466abb80d3b1ac3420a89aa433cd5eb246245
EC2 Default User <ec2-user@ip-172-31-31-207.ap-northeast-1.compute.internal>
2024-03-13 14:56:30 +0000

    [Add]ゲストログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,15 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  devise_scope :user do
-    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
-  end
-
   root to: 'homes#top'
   get 'home/about' => 'homes#about', as: 'about'
 
   resources :users, only: [:show, :index, :edit, :update]
   resources :books, only: [:show, :create, :index, :edit, :update, :destroy]
+
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  end
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
