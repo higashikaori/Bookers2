fe8ba80d2ba27b0066077df00a5cc90aa7745cb3
plant0322 <159232279+plant0322@users.noreply.github.com>
2024-03-19 08:25:57 +0000

    [Add]フォロー機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,12 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+
   validates :name, uniqueness: { scope: :name }, length: { in: 2..20 }
   validates :introduction, length: { maximum: 50 }
 
@@ -18,6 +24,18 @@ class User < ApplicationRecord
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     profile_image.variant(resize_to_limit: [width, height]).processed
+  end
+
+  def follow(user)
+    relationships.create(followed_id: user.id)
+  end
+
+  def unfollow(user)
+    relationships.find_by(followed_id: user.id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
   end
 
 GUEST_USER_EMAIL = "guest@example.com"
