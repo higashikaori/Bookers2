96b5503deaaa495b09cfc8dd4b9a8c8cc1e608cf
plant0322 <159232279+plant0322@users.noreply.github.com>
2024-03-19 15:03:29 +0000

    [Add]閲覧数

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,9 @@ class BooksController < ApplicationController
   def show
     @new_book = Book.new
     @book = Book.find(params[:id])
+    unless ViewCount.find_by(user_id: current_user.id, book_id: @book.id)
+      current_user.view_counts.create(book_id: @book.id)
+    end
   end
 
   def index
