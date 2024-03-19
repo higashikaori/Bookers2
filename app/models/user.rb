2078672b19922b0455b8df9139ff505595f65ef1
plant0322 <159232279+plant0322@users.noreply.github.com>
2024-03-19 09:22:00 +0000

    [Add]DM機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,10 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :followed
   has_many :followers, through: :reverse_of_relationships, source: :follower
 
+  has_many :user_rooms, dependent: :destroy
+  has_many :chats, dependent: :destroy
+  has_many :rooms, dependent: :destroy
+
   validates :name, uniqueness: { scope: :name }, length: { in: 2..20 }
   validates :introduction, length: { maximum: 50 }
 
