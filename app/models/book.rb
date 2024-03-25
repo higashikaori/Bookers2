a3d51e9d8de2c429b4bbb8ea7e80c36282efa31d
plant0322 <159232279+plant0322@users.noreply.github.com>
2024-03-25 09:21:07 +0000

    [Add]タグ

--- app/models/book.rb
+++ app/models/book.rb
@@ -22,4 +22,7 @@ class Book < ApplicationRecord
   scope :created_thisweek, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) }
   scope :created_lastweek, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) }
 
+  scope :latest, -> {order(created_at: :desc)}
+  scope :star_count, -> {order(star: :desc)}
+
 end
